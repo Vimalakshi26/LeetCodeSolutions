124375d2a227ed3771afb9a68ed1a8286fa16e74
Vimalakshi26 <164524604+Vimalakshi26@users.noreply.github.com>
2026-08-24 17:56:24 +0530

    Time: 20 ms (12.25%) | Memory: 50.8 MB (8.93%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap< String, List<String>> map = new HashMap<>();
+        for(String str: strs){
+            int[] freq = new int[26];
+            for(char ch: str.toCharArray()){
+                freq[ch - 'a']++;
+            }
+            StringBuilder key = new StringBuilder();
+            for(int i = 0; i<26; i++){
+                key.append(freq[i]).append('#');
+            }
+            if(!map.containsKey(key.toString())){
+                map.put(key.toString(), new ArrayList<>());
+            }
+            map.get(key.toString()).add(str);
+        }
+         return new ArrayList<>(map.values());
+    }
+}
